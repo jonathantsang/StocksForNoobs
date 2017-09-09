3fd43d17a6969575cfa8caad764641faf2326b63
jonathantsang <j26tsang@gmail.com>
2017-09-09 11:09:48 -0400

    server

--- index.js
+++ index.js
@@ -5,10 +5,31 @@ var cheerio = require('cheerio');
 
 app.use(express.static('.'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000');
+  var names = Scrape();
+  console.log(names);
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+function Scrape(){
+  var names = [];
+  var query = "bread"; // Hardcode for now
+  var url = "https://www.google.com/finance?noIL=1&q=" + query;
+
+  request(url, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+        $('td.symbol').each(function(i, element){
+          var a = $(this);
+          var ticker = /[A-Z][A-Z][A-Z][A-Z]?/
+        var res = ticker.exec(a.text());
+          if(res != null){
+            names.push(res[0]);
+            console.log(res[0]);
+            }
+        });
+      }
+    console.log(names);
+  });
+  return names;
+}
--- scrape.js
+++ scrape.js
@@ -24,6 +24,4 @@ function Scrape(){
   return names;
 }
 
-Scrape();
-
 
