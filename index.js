3aaa16950fbd9c0c060d642593c009856423a5e7
jonathantsang <j26tsang@gmail.com>
2017-09-11 18:40:50 -0400

    slow api call

--- index.js
+++ index.js
@@ -39,9 +39,11 @@ function Scrape(word){
 	var query = word; // Hardcode for now
 	var url = "https://www.google.com/finance?noIL=1&q=" + query;
 	tickers = [];
+    var counter = 0;
 	request(url, function (error, response, html) {
     if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
+        // Getting tickers
         $('td.symbol').each(function(i, element){
         	var a = $(this);
         	var ticker = /[A-Z]+/
@@ -54,16 +56,23 @@ function Scrape(word){
         			// hardcode if it is length 3 or 4 keep it (not 5)
         			if(res[0].length == 3 || res[0].length == 4){
         				data.names.push(res[0]);
+                        data.descriptions.push(null);
+                        tickers[res[0]] = counter;
+                        counter++;
+                        getDescrips(res[0], data);
         			} else {
+                        counter++;
         				data.names.push(null);
         			}
         		}	
         	} else {
+                counter++;
         		data.names.push(null);
         	}
         });
+        console.log(data.names);
 
-        // Get the title
+        // Get the stock full name
         $('td.localName').each(function(i, element){
         	var a = $(this);
         	var res = a.text();
@@ -73,6 +82,7 @@ function Scrape(word){
         		data.titles.push(null);
         	}
         });
+        console.log(data.titles);
 
         // Get the prices 
         $('td.price').each(function(i, element){
@@ -86,6 +96,7 @@ function Scrape(word){
         		data.prices.push(null);
         	}
         });
+        console.log(data.prices);
 
         // Get the marketCaps
         $('td.mktCap').each(function(i, element){
@@ -98,36 +109,67 @@ function Scrape(word){
         		data.marketCaps.push(null);
         	}
         });
+        console.log(data.marketCaps);
 
-        // Get the descriptions
-        $('div.description').each(function(i, element){
+        // Get the descriptions 
+        /*$('div.description').each(function(i, element){
         	var a = $(this);
-        	var ticker = /.*/
+        	var ticker = /.
         	var res = ticker.exec(a.text());
         	if(res != null){
         		data.descriptions.push(res[0]);
         	} else {
         		data.descriptions.push(null);
         	}
+        });  */      
+    }
+
+    // Send the data to the front end
+    slow(data);
+    });
+}
+
+function getDescrips(query, data){
+    var url = "https://www.google.com/finance?q=" + query;
+    request(url, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+         $('div.companySummary').each(function(i, element){
+            var a = $(this);
+            var res = a.text();
+            if(res != null){
+                data.descriptions[tickers[query]] = res;
+            } else {
+                data.descriptions.push(null);
+            }
         });
     }
+    console.log(data.descriptions);
+    })
+}
+
+function slow(data){
+    setTimeout( function(){
+    console.log( 'slow api call' );
     // Validate the data by checking each row
     var resultsPerPage = 15;
     var alteredData = {names : [], titles : [], prices : [], marketCaps : [], descriptions : []}
     for(var i = 0; i < resultsPerPage; i++){
-    	if(data.names[i] != null && data.titles[i] != null && data.prices[i] != null && data.marketCaps[i] != null && data.descriptions[i] != null){
-    		alteredData.names.push(data.names[i]);
-    		alteredData.titles.push(data.titles[i]);
-    		alteredData.prices.push(data.prices[i]);
-    		alteredData.marketCaps.push(data.marketCaps[i]);
-    		alteredData.descriptions.push(data.descriptions[i]);
-    	}
+        if(data.names[i] != null && data.titles[i] != null && data.prices[i] != null && data.marketCaps[i] != null && data.descriptions[i] != null){
+            // Need to check if the callback for the descriptions comes back
+            alteredData.names.push(data.names[i]);
+            alteredData.titles.push(data.titles[i]);
+            alteredData.prices.push(data.prices[i]);
+            alteredData.marketCaps.push(data.marketCaps[i]);
+            alteredData.descriptions.push(data.descriptions[i]);
+        }
     }
 
-    // Send the data to the front end
-	console.log("queried");
-	io.emit('tickersreceived', {tickerData: alteredData.names, titleData: alteredData.titles, priceData: alteredData.prices, marketCapData: alteredData.marketCaps,
-		descriptionsData : alteredData.descriptions });
-  });
+    io.emit('tickersreceived', {tickerData: alteredData.names, titleData: alteredData.titles, priceData: alteredData.prices, marketCapData: alteredData.marketCaps,
+        descriptionsData : alteredData.descriptions });
+    console.log("sent");
+  }, 3000 );
+    
 }
+
 
